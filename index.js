31a7c56af218587a0d079e3b50cadc4c43632574
Chattarika Bunchongpru <saiparn@Chattarikas-MacBook-Air.local>
2023-09-13 13:16:08 +0700

    updation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-function printName(name = "Men") {
-  console.log(name);
+const letters = "ABDxyz";
+for (let i = 0; i < 3; i++) {
+  for (let j = 3; j < 6; j++) {
+    console.log(`${letters[i]}${letters[j]}`);
+  }
 }
-
-printName("John" );
